f524a37b3877a2043906ce0d1d5dacfe4307a2e4
Fuzzwah <fuzzwah@github.com>
2018-04-18 07:45:50 +1000

    Create iRacing-My-Results-Highlighter.meta.js

---
+++ iRacing-My-Results-Highlighter.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @namespace     http://www.fuzzwahracing.com/p/results-highlighter.html
+// @name          iRacing My Results Highlighter
+// @description   Highlights the rows of your entries in the iRacing event results table
+// @include       http://members.iracing.com/membersite/member/EventResult.do*
+// @version       1.18.04.18.02
+// @author        fuzzwah
+// @copyright     2018+, fuzzwah (https://github.com/fuzzwah)
+// @license       MIT; https://raw.githubusercontent.com/fuzzwah/iRacing-My-Results-Highlighter/master/LICENSE
+// @homepageURL   http://www.fuzzwahracing.com/p/results-highlighter.html
+// @updateURL     https://raw.githubusercontent.com/fuzzwah/iRacing-My-Results-Highlighter/master/iRacing-My-Results-Highlighter.meta.js
+// ==/UserScript==
