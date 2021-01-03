e13ad20595cefd62a3b3496ed739df83487cbe9e
Fuzzwah <fuzzwah@github.com>
2021-01-04 07:06:24 +1100

    increase to max of 14 drivers

--- iRacing-My-Results-Highlighter.user.js
+++ iRacing-My-Results-Highlighter.user.js
@@ -3,7 +3,7 @@
 // @name          iRacing My Results Highlighter
 // @description   Highlights the rows of your, and optionally other selected drivers, in the iRacing event results table
 // @include       *://members.iracing.com/membersite/member/EventResult.do*
-// @version       2.20.11.12.03
+// @version       2.21.01.04.01
 // @author        fuzzwah
 // @copyright     2018+, fuzzwah (https://github.com/fuzzwah)
 // @license       MIT; https://raw.githubusercontent.com/fuzzwah/iRacing-My-Results-Highlighter/master/LICENSE
@@ -298,11 +298,59 @@ function actionHighlight(jNode) {
                 'class': 'color',
                 'size': 8
             },
+            'driver13_name': {
+                'section': [GM_config.create('Driver 13'), ''],
+                'labelPos': 'left',
+                'type': 'text',
+                'default': '',
+                'class': 'name',
+                'size': 30
+            },
+            'driver13_custid': {
+                'labelPos': 'left',
+                'type': 'int',
+                'default': '',
+                'class': 'custid',
+                'size': 12
+            },
+            'driver13_color': {
+                'labelPos': 'left',
+                'type': 'text',
+                'default': '',
+                'class': 'color',
+                'size': 8
+            },
+            'driver14_name': {
+                'section': [GM_config.create('Driver 14'), ''],
+                'labelPos': 'left',
+                'type': 'text',
+                'default': '',
+                'class': 'name',
+                'size': 30
+            },
+            'driver14_custid': {
+                'labelPos': 'left',
+                'type': 'int',
+                'default': '',
+                'class': 'custid',
+                'size': 12
+            },
+            'driver14_color': {
+                'labelPos': 'left',
+                'type': 'text',
+                'default': '',
+                'class': 'color',
+                'size': 8
+            },
         },
         'css': '.name {float: left; width: 50%; margin-top: 5px;} .custid {float: left; width: 30%; margin-top: 5px;} .color {float: left; width: 20%; margin-top: 5px;}'
     });
 
     var stored_drivers = GM_config.read();
+
+    GM_config.onSave = function() {
+         alert('Driver list saved');
+    };
 
     GM_registerMenuCommand("Set drivers to highlight", function() { GM_config.open(); });
 
@@ -346,7 +394,8 @@ function actionHighlight(jNode) {
         var drivers = [];
     }
 
-    for (i=1; i<11; i++) {
+    console.log(Object.keys(stored_drivers).length/3)
+    for (i=1; i<=Object.keys(stored_drivers).length/3; i++) {
         drivers.push([stored_drivers["driver" + i + "_name"], stored_drivers["driver" + i + "_custid"].toString(), stored_drivers["driver" + i + "_color"]]);
     }
 
